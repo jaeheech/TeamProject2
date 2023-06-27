df241a82eb1800b9305efb0b971503103b1e3e5a
jaeheech <ks2451ss1@naver.com>
2023-06-27 14:07:15 +0900

    Teamproject

--- static/js/index.js
+++ static/js/index.js
@@ -1,63 +1,50 @@
-const socket = io();
+const express = require("express");
+const app = express();
+const http = require("http").createServer(app);
+const io = require("socket.io")(http);
 
-/* 접속 되었을 때 실행 */
-socket.on("connect", function () {
-  /* 이름을 입력받고 */
-  var name = prompt("반갑습니다!", "");
+app.use(express.static("static"));
 
-  /* 이름이 빈칸인 경우 */
-  if (!name) {
-    name = "익명";
-  }
-
-  /* 서버에 새로운 유저가 왔다고 알림 */
-  socket.emit("newUser", name);
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/static/html/index.html");
 });
 
-/* 서버로부터 데이터 받은 경우 */
-socket.on("update", function (data) {
-  var chat = document.getElementById("chat");
+io.on("connection", (socket) => {
+  console.log("a user connected");
+  let users = [];
 
-  var message = document.createElement("div");
-  var node = document.createTextNode(`${data.name}: ${data.message}`);
-  var className = "";
+  socket.on("login", (data) => {
+    const user = { socketId: socket.id, ...data };
+    users.push(user);
+    socket.broadcast.emit("update", { type: "connect", ...user });
+  });
 
-  // 타입에 따라 적용할 클래스를 다르게 지정
-  switch (data.type) {
-    case "message":
-      className = "other";
-      break;
+  socket.on("sendMessage", (message) => {
+    const sender = users.find((user) => user.socketId === socket.id);
+    socket.broadcast.emit("update", {
+      type: "message",
+      nickname: sender.nickname,
+      displayName: sender.displayName,
+      message,
+    });
+  });
 
-    case "connect":
-      className = "connect";
-      break;
-
-    case "disconnect":
-      className = "disconnect";
-      break;
-  }
-
-  message.classList.add(className);
-  message.appendChild(node);
-  chat.appendChild(message);
+  socket.on("disconnect", () => {
+    const disconnectedUser = users.find((user) => user.socketId === socket.id);
+    if (disconnectedUser) {
+      users = users.filter((user) => user.socketId !== socket.id);
+      const nickname = disconnectedUser.nickname;
+      const displayName = disconnectedUser.displayName;
+      socket.broadcast.emit("update", {
+        type: "disconnect",
+        nickname,
+        displayName,
+      });
+    }
+  });
 });
 
-/* 메시지 전송 함수 */
-function send() {
-  // 입력되어있는 데이터 가져오기
-  var message = document.getElementById("test").value;
-
-  // 가져왔으니 데이터 빈칸으로 변경
-  document.getElementById("test").value = "";
-
-  // 내가 전송할 메시지 클라이언트에게 표시
-  var chat = document.getElementById("chat");
-  var msg = document.createElement("div");
-  var node = document.createTextNode(message);
-  msg.classList.add("me");
-  msg.appendChild(node);
-  chat.appendChild(msg);
-
-  // 서버로 message 이벤트 전달 + 데이터와 함께
-  socket.emit("message", { type: "message", message: message });
-}
+const port = process.env.PORT || 3000;
+http.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
